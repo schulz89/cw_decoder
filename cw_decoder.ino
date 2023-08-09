52352bee499a7f664195b834e76c29f94b4fbdf7
Victor Schulz <schulz89@gmail.com>
2023-08-09 15:41:46 +0900

    added license header

--- cw_decoder.ino
+++ cw_decoder.ino
@@ -1,3 +1,20 @@
+// Arduino program to decode CW beacon in morse code
+// Copyright (C) 2023 Victor Hugo Schulz
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 #define CW_IN 12
 #define BUFFER_SIZE 500L
 #define TIMER_SETTING 15000 // 15000 corresponds to 60ms
